6974fa48e9dd8c1b91fa543971fe3ed6e6bd1ef6
Mishima-Hiromi <hiromishi21@gmail.com>
2025-12-30 03:14:59 +0900

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "split-bill-app-v1.99";
+const CACHE_NAME = "split-bill-app-v2.00";
 // オフラインで表示するために必要なファイルの一覧
 const urlsToCache = ["index.html", "manifest.json", "logo512.png"];
 
