ca4a3c6b81a876fa9b95ef43fafa80d3181776b1
Mishima-Hiromi <hiromishi21@gmail.com>
2025-12-29 14:45:25 +0900

    manifestとserviceworkerを追加

---
+++ public/sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "split-bill-app-v1";
+// オフラインで表示するために必要なファイルの一覧
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./logo192.png",
+  "./logo512.png",
+];
+
+// ① インストール時にファイルを保存
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// ② ネットがない時はキャッシュから読み込む
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // キャッシュがあればそれを返し、なければネットに取りに行く
+      return response || fetch(event.request);
+    })
+  );
+});
