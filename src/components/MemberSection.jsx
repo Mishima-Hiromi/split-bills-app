31a22ee6a7669183f0ae3b079e88c206d170d5c2
Mishima-Hiromi <hiromishi21@gmail.com>
2025-12-30 12:48:06 +0900

    レイアウトを調整

--- src/components/MemberSection.jsx
+++ src/components/MemberSection.jsx
@@ -22,7 +22,7 @@ function MemberSection({
     display: "flex",
     flexDirection: "column",
     overflow: "hidden",
-    maxHeight: isMobile ? "700px" : "100%",
+    maxHeight: isMobile ? "1000px" : "100%",
   };
 
   const inputStyle = {
